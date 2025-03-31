8b11854ed5b1d07a92f552f642efdf45e007c955
AkumaBlazeX <sanathkumar.data@gmail.com>
2025-03-31 22:47:11 +0530

    Fix: Remove base URL configurations and update homepage

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/portfolio-sanath">
+        <BrowserRouter>
           <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex-grow">
